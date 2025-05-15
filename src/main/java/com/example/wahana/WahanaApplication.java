667a61104e4a0d56d69255df1902115f42f5b72c
diaspradana <daniswaradias87@gmail.com>
2025-05-15 17:19:43 +0700

    first commit

---
+++ src/main/java/com/example/wahana/WahanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.wahana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WahanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WahanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wahana/WahanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wahana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WahanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
