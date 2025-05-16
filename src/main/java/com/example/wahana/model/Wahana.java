9b0b5aed410660c8d5370e60b27f0bf82d56de8c
Bernaldiatma <atmaa2180@gmail.com>
2025-05-16 17:06:24 +0700

    Menambahkan file Wahana dan WahanaService

---
+++ src/main/java/com/example/wahana/model/Wahana.java
@@ -0,0 +1,72 @@
+package com.example.wahana.model;
+
+import com.example.wahana.WahanaApplication;
+
+public class Wahana {
+    private Long id;
+    private String namaWahana;
+    private String deskripsi;
+    private String kategori;
+    private double harga;
+    private String gambar;
+
+    public Wahana() {
+    }
+
+    public Wahana(Long id,String namaWahana, String deskripsi, String kategori ,double harga, String gambar){
+        this.id = id;
+        this.namaWahana = namaWahana;
+        this.deskripsi = deskripsi;
+        this.kategori = kategori;
+        this.harga = harga;
+        this.gambar = gambar;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getNamaWahana(){
+        return namaWahana;
+    }
+
+    public void setNamaWahana(String namaWahana){
+        this.namaWahana = namaWahana;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getKategori () {
+        return kategori;
+    }
+
+    public void setKategori (String kategori) {
+        this.kategori = kategori;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public String getGambar () {
+        return gambar;
+    }
+
+    public void setGambar(String gambar) {
+        this.gambar = gambar;
+    }
+}
---
+++ src/main/java/com/example/wahana/service/WahanaService.java
@@ -0,0 +1,84 @@
+package com.example.wahana.service;
+
+import com.example.wahana.model.Wahana;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WahanaService {
+    private final List<Wahana> wahanaList = new ArrayList<>();
+    private Long idCounter = 2L;
+
+    public WahanaService() {
+        // Data Dummy
+        wahanaList.add(new Wahana(1L, "Bianglala", "Iso muter bjir", "Semua Umur", 30000, "\uD83C\uDFA1"));
+        wahanaList.add(new Wahana(2L, "Rumah Hantu", "Alamak takutnyooo", "Dewasa", 25000, "\uD83D\uDC7B"));
+    }
+
+    // Menampilkan semua list wahana
+    public List<Wahana> getAllWahana(){
+        return wahanaList;
+    }
+
+    // Searching dan Sorting
+    public  List<Wahana> carWahana(String keyword, String sortBy, String sortDir){
+        List<Wahana> result = wahanaList;
+
+        if (keyword != null && !keyword.isEmpty()) {
+            final String lowerKeyword = keyword.toLowerCase();
+            result = result.stream().filter(w -> w.getNamaWahana().toLowerCase().contains(lowerKeyword)).toList();
+        }
+
+        // Sorting wahana
+        if (sortBy != null && !sortBy.isEmpty()){
+            Comparator<Wahana> comparator;
+
+            switch (sortBy) {
+                case "namaWahana":
+                    comparator = Comparator.comparing(Wahana::getNamaWahana);
+                    break;
+                case "kategoriWahana":
+                    comparator = Comparator.comparing(Wahana::getKategori);
+                    break;
+                case "id":
+                default:
+                    comparator = Comparator.comparing(Wahana::getId);
+                    break;
+            }
+
+            if ("desc".equals(sortDir)){
+                comparator = comparator.reversed();
+            }
+
+            result = result.stream().sorted(comparator).collect(Collectors.toList());
+        }
+
+        return result;
+    }
+
+    public Wahana saveWahana(Wahana wahana) {
+        if (wahana.getId() == null){
+            wahana.setId(idCounter++);
+            wahanaList.add(wahana);
+        }
+        return wahana;
+    }
+
+    public Wahana getWahanaById(Long id){
+        return wahanaList.stream().filter(w -> w.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    public void updateWahana(Wahana wahana){
+        wahanaList.replaceAll(w -> w.getId().equals(wahana.getId()) ? wahana : w);
+    }
+
+    public void deleteWahana(Long id){
+        wahanaList.removeIf(w -> w.getId().equals(id));
+    }
+
+    public Wahana getWahanaByID(Long id){
+        return wahanaList.stream().filter(w -> w.getId().equals(id)).findFirst().orElse(null);
+    }
+}
